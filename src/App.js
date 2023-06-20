d4e0bd40f7cd17423ae4415668a92aa489fec6aa
ManuelDev <manueldeveloper17@gmail.com>
2023-06-20 13:18:05 -0500

    (Feature) Add routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+// Pages
+import { Home } from "./pages/Home";
+import { Products } from "./pages/Products";
+import { ProductDetails } from "./pages/ProductDetails";
+import { Search } from "./pages/Search";
+import { NotFound } from "./pages/NotFound";
+
+// Components
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+
+// Layout
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      {/* Children component */}
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+// Routes
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetails />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      // If no route matches, render the NotFound page
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
